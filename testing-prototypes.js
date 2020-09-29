9e3b7b1e1788ce010dcc19bbcef33f4a154ea47a
Olivier Girardot <ogmusik@gmail.com>
2020-09-29 16:56:10 +0200

    also added a small tutorial on prototypes, that need to be sorted

--- testing-prototypes.js
+++ testing-prototypes.js
@@ -2,17 +2,30 @@ function Burger() {
     this.ingredients = {};
 };
 
-const veggie = new Burger(
-);
+const veggie = new Burger();
 
 veggie.ingredients["pattie"] = "beyond meat";
 veggie.ingredients["sauce"] = "ketchup";
 
+// a prototype is an object that is attached to an Object Constructor but is not part of it
+// In the console.logs bellow we can see that to call the prototype we have to specifically call it
+// If we call the object instance, the information stored in the prototype is not
+Burger.prototype.drink = "cola"
 
+
+// see how this works:
+console.log(veggie);
+console.log(veggie.drink);
+
+const vegan = new Burger();
+
+console.log(vegan);
+console.log(vegan.drink);
+
+
+// Here is an example of how to use a prototype as a funtion
 Burger.prototype.order = function() {
-    console.log(`You have ordered a burger with ${this.ingredients["pattie"]} and ${this.ingredients["sauce"]}`)
+    return `You have ordered a burger with ${this.ingredients["pattie"]} and ${this.ingredients["sauce"]}. Your drink is ${this.drink}`;
 };
 
-console.log(veggie);
-
-veggie.order();
+console.log(veggie.order());
