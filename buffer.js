8c8f6040c8ee8bed08a3e6670184796eebe33c69
Olivier Girardot <ogmusik@gmail.com>
2020-10-01 11:44:02 +0200

    did some more testing with the buffer again

--- buffer.js
+++ buffer.js
@@ -17,6 +17,11 @@ buf.toJSON().data.forEach(element => {
     console.log(element.charCodeAt(0).toString(2))
 });
 
+// overwritting. As we ca see in the log the buffer only stores 5 characters because of
+// when we defined the buffer with "hello": 5 characters
+buf.write("goodbye")
+console.log(buf.toString());
+
 // testing the to.String(2) converter method that converts a number into a binary.
 // const num = 111;
 // console.log(num.toString(2))
